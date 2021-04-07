878defa48813edcf3231b737bba6e13899bc53fa
Beatriz <beatrizdepaulavaz@gmail.com>
2021-04-07 23:48:23 +0100

    Front page

--- bookmanagement_app/__init__.py
+++ bookmanagement_app/__init__.py
@@ -1,2 +1,18 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+
+db = SQLAlchemy()
+
+def create_app():
+
+    app = Flask(__name__)
+
+    Bootstrap(app)
+    db.init_app(app)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
+    return app
